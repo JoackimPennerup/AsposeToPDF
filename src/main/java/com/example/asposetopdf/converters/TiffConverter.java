7c781a48e62500e95fdf6b7337a19c52a2db7384
Joackim Pennerup <joackim.pennerup@idainfront.se>
2025-10-13 11:46:13 +0200

    Handle 1 bit per pixel

--- src/main/java/com/example/asposetopdf/converters/TiffConverter.java
+++ src/main/java/com/example/asposetopdf/converters/TiffConverter.java
@@ -1,16 +1,18 @@
 package com.example.asposetopdf.converters;
 
 import com.aspose.imaging.Image;
-import com.aspose.imaging.fileformats.png.PngColorType;
+import com.aspose.imaging.ResolutionSetting;
 import com.aspose.imaging.fileformats.tiff.TiffFrame;
 import com.aspose.imaging.fileformats.tiff.TiffImage;
+import com.aspose.imaging.fileformats.tiff.enums.TiffExpectedFormat;
 import com.aspose.imaging.imageoptions.PngOptions;
+import com.aspose.imaging.imageoptions.TiffOptions;
 import com.aspose.pdf.Document;
+import com.aspose.pdf.ImageFilterType;
 import com.aspose.pdf.MarginInfo;
 import com.aspose.pdf.Page;
 import com.example.asposetopdf.detect.FileType;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.nio.file.Path;
 
@@ -44,21 +46,24 @@ public class TiffConverter extends ImageConverter {
                     setPageDimensions(page, frame.getWidth(), frame.getHeight(),
                             frame.getHorizontalResolution(), frame.getVerticalResolution());
 
+                    double widthPoints = page.getPageInfo().getWidth();
+                    double heightPoints = page.getPageInfo().getHeight();
+
+                    if (frame.getBitsPerPixel() == 1) {
+                        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                             TiffOptions tiffOptions = new TiffOptions(TiffExpectedFormat.TiffCcittFax4)) {
+                            tiffOptions.setResolutionSettings(new ResolutionSetting(frame.getHorizontalResolution(), frame.getVerticalResolution()));
+                            frame.save(baos, tiffOptions);
+                            embedImage(page, baos.toByteArray(), widthPoints, heightPoints, ImageFilterType.CCITTFax);
+                        }
+                        continue;
+                    }
+
                     try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
                         PngOptions options = new PngOptions();
                         options.setCompressionLevel(0);
-                        if (frame.getBitsPerPixel() == 1) {
-                            options.setColorType(PngColorType.Grayscale);
-                            options.setBitDepth((byte) 1);
-                        }
                         frame.save(baos, options);
-
-                        com.aspose.pdf.Image pdfImage = new com.aspose.pdf.Image();
-                        pdfImage.setImageStream(new ByteArrayInputStream(baos.toByteArray()));
-                        pdfImage.setFixWidth(page.getPageInfo().getWidth());
-                        pdfImage.setFixHeight(page.getPageInfo().getHeight());
-                        pdfImage.setIsApplyResolution(true);
-                        page.getParagraphs().add(pdfImage);
+                        embedImage(page, baos.toByteArray(), widthPoints, heightPoints);
                     }
                 }
 
@@ -68,4 +73,5 @@ public class TiffConverter extends ImageConverter {
             }
         }
     }
+
 }
